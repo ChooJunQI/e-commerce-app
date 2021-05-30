36282b840bbcb07da5529082ae2414fc5309adb1
Junqi <cjqchoojunqi@gmail.com>
2021-05-30 09:38:32 +0800

    broken up homepage into smaller components and finish styling menu items

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./homepage.style.scss";
+
+import Directory from "../../components/menu-item/directory/directory";
+
+
+
+
+ const HomePage = () =>{
+   return <div className="homepage">
+        <Directory/>
+    </div>
+}
+
+
+export default HomePage;
